1e6411ad25df3b6efa89ff4c3bac9c99a552d33a
YuliiaLukianiuk <lukianiky@gmail.com>
2020-11-04 21:11:39 +0200

    Реализован выпадающий список в меню

--- main.js
+++ main.js
@@ -7,3 +7,30 @@ $(function () {
     wrap: false
   });
 });
+
+
+$('.navigation-wrap, .shadow-layer').hide();
+$('.navbar-btn').click(function(){
+    $('.navigation-wrap, .shadow-layer').show();
+});
+
+$('.navigation-block-img').on('click', function(){
+  $(this).closest('.navigation-wrap').hide();
+  $("div.shadow-layer").hide();
+});
+
+$('.category-block-wrap').hide();
+$('.class-unit-minus-3').hide();
+$('.class-unit-plus-3').click(function(){
+  $('.class-unit-plus-3').hide();
+   $('.class-unit-minus-3').show();
+    $('.category-block-wrap').show();
+    $('.filter-wrap').css('top','450px');
+});
+
+$('.class-unit-minus-3').click(function(){
+  $('.category-block-wrap').hide();
+  $('.filter-wrap').css('top','254px');
+   $('.class-unit-minus-3').hide();
+   $('.class-unit-plus-3').show();
+});
